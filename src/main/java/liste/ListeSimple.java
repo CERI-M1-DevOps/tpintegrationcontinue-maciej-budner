06730ed4289b39a992d97a0d03a5385168f78fd9
Maciej <maciej.budner@alumni.univ-avignon.fr>
2025-09-18 15:55:58 +0200

    correction pour avoir 100% de coverage

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -120,26 +120,30 @@ public class ListeSimple {
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 == r2){
             return;}
-        Noeud precedentR1;
-        Noeud precedentR2;
-        if (r1 != tete && r2 != tete) {
-            precedentR1 = getPrecedent(r1);
-            precedentR2 = getPrecedent(r2);
-            precedentR1.setSuivant(r2);
-            precedentR2.setSuivant(r1);
-        } else if (r1 == tete) {
-            precedentR2 = getPrecedent(r2);
-            precedentR2.setSuivant(tete);
-            tete = r2;
+        else{
+            Noeud precedentR1;
+            Noeud precedentR2;
+            if (r1 != tete && r2 != tete) {
+                precedentR1 = getPrecedent(r1);
+                precedentR2 = getPrecedent(r2);
+                precedentR1.setSuivant(r2);
+                precedentR2.setSuivant(r1);
+            }
+            else if (r1 == tete) {
+                precedentR2 = getPrecedent(r2);
+                precedentR2.setSuivant(tete);
+                tete = r2;
+            }
+            //else if (r2 == tete) { //d'apres sonarcloud ce test sera toujour vrai
+            else{
+                precedentR1 = getPrecedent(r1);
+                precedentR1.setSuivant(tete);
+                tete = r1;
+            }
+            Noeud temp = r2.getSuivant();
+            r2.setSuivant(r1.getSuivant());
+            r1.setSuivant(temp);
         }
-        else if (r2 == tete) {
-            precedentR1 = getPrecedent(r1);
-            precedentR1.setSuivant(tete);
-            tete = r1;
-        }
-        Noeud temp = r2.getSuivant();
-        r2.setSuivant(r1.getSuivant());
-        r1.setSuivant(temp);
     }
 
 }
