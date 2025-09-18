bdcf56f9319b38132e6d2793041cd062d43fa03b
Maciej <maciej.budner@alumni.univ-avignon.fr>
2025-09-18 16:43:20 +0200

    rajoue des comentaire pour chaque fonction de la class

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -3,16 +3,28 @@ package liste;
 public class ListeSimple {
     private long size;
     Noeud tete;
-
+    /**
+     * Description de la méthode
+     * recupére la longueur de la liste
+     * @return Description de la valeur retournée
+     */
     public long getSize() {
         return size;
     }
-
+    /**
+     * Description de la méthode
+     * ajoute un noeud dans la list
+     * @return Description de la valeur retournée
+     */
     public void ajout(int element) {
         tete = new Noeud(element, tete);
         size++;
     }
-
+    /**
+     * Description de la méthode
+     * modifie le premier noeud qui a l'element que l'on a donner
+     * @return Description de la valeur retournée
+     */
     public void modifiePremier(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null && courant.getElement() != element){
@@ -20,7 +32,11 @@ public class ListeSimple {
         if (courant != null){
             courant.setElement(nouvelleValeur);}
     }
-
+    /**
+     * Description de la méthode
+     * modifie toute les element des noeud qui sont egal a l'element rentré
+     * @return Description de la valeur retournée
+     */
     public void modifieTous(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null) {
@@ -29,7 +45,11 @@ public class ListeSimple {
             courant = courant.getSuivant();
         }
     }
-
+    /**
+     * Description de la méthode
+     * affiche la liste sous forme d'un string
+     * @return Description de la valeur retournée
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder("ListeSimple(");
         Noeud n = tete;
@@ -42,7 +62,11 @@ public class ListeSimple {
         sb.append(")");
         return sb.toString();
     }
-
+    /**
+     * Description de la méthode
+     * suprime le premier noeud de la tete
+     * @return Description de la valeur retournée
+     */
     public void supprimePremier(Object element) {
         if (tete != null) {
             if (tete.getElement() == element) {
@@ -62,11 +86,19 @@ public class ListeSimple {
             }
         }
     }
-
+    /**
+     * Description de la méthode
+     * inisialise la supresion de tout la liste recurcivement
+     * @return Description de la valeur retournée
+     */
     public void supprimeTous(int element) {
        tete = supprimeTousRecurs(element, tete);
     }
-
+    /**
+     * Description de la méthode
+     * suprime tout les noeud de la liste recurcivement
+     * @return Description de la valeur retournée
+     */
     public Noeud supprimeTousRecurs(Object element, Noeud tete) {
         if (tete != null) {
             Noeud suiteListe = supprimeTousRecurs(element, tete.getSuivant());
@@ -79,7 +111,11 @@ public class ListeSimple {
             }
         } else return null;
     }
-
+    /**
+     * Description de la méthode
+     * recupére l'avant dernier noeud de la liste
+     * @return Description de la valeur retournée
+     */
     public Noeud getAvantDernier() {
         if (tete == null || tete.getSuivant() == null)
             return null;
@@ -93,7 +129,11 @@ public class ListeSimple {
             return courant;
         }
     }
-
+    /**
+     * Description de la méthode
+     * inverse l'ordre de la liste
+     * @return Description de la valeur retournée
+     */
     public void inverser() {
         Noeud precedent = null;
         Noeud courant = tete;
@@ -105,7 +145,11 @@ public class ListeSimple {
         }
         tete = precedent;
     }
-
+    /**
+     * Description de la méthode
+     * recupére le noeud precedent
+     * @return Description de la valeur retournée
+     */
     public Noeud getPrecedent(Noeud r) {
     // la liste n'est pas vide puisqu'on transmet un Node de la liste et le Node existe obligatoirement
         Noeud precedent = tete;
@@ -116,7 +160,11 @@ public class ListeSimple {
         }
         return precedent;
     }
-
+    /**
+     * Description de la méthode
+     * echange de position entre les 2 noeud de la liste
+     * @return Description de la valeur retournée
+     */
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 != r2){
 
