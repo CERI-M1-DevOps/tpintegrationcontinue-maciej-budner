17731aeb89ff506a78ed5377de6e2684819ffe25
Maciej <maciej.budner@alumni.univ-avignon.fr>
2025-09-24 18:35:34 +0200

    modifier les commantaire car il etais pas bon return sur des fonction void et fise versa

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -1,41 +1,51 @@
 package liste;
-
+/**
+ * Représente une liste chaînée simple.
+ */
 public class ListeSimple {
     private long size;
     Noeud tete;
+
     /**
      * Description de la méthode
      * recupére la longueur de la liste
-     * @return Description de la valeur retournée
+     * @return taille de la liste
      */
     public long getSize() {
         return size;
     }
+
     /**
      * Description de la méthode
      * ajoute un noeud dans la list
-     * @return Description de la valeur retournée
+     * @param element l'élément à rajouter
      */
     public void ajout(int element) {
         tete = new Noeud(element, tete);
         size++;
     }
+
     /**
      * Description de la méthode
      * modifie le premier noeud qui a l'element que l'on a donner
-     * @return Description de la valeur retournée
+     * @param element l'élément à rechercher
+     * @param nouvelleValeur la nouvelle valeur à mettre
      */
     public void modifiePremier(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null && courant.getElement() != element){
-            courant = courant.getSuivant();}
+            courant = courant.getSuivant();
+        }
         if (courant != null){
-            courant.setElement(nouvelleValeur);}
+            courant.setElement(nouvelleValeur);
+        }
     }
+
     /**
      * Description de la méthode
      * modifie toute les element des noeud qui sont egal a l'element rentré
-     * @return Description de la valeur retournée
+     * @param element l'élément à rechercher
+     * @param nouvelleValeur la nouvelle valeur à mettre
      */
     public void modifieTous(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
@@ -45,10 +55,11 @@ public class ListeSimple {
             courant = courant.getSuivant();
         }
     }
+
     /**
      * Description de la méthode
      * affiche la liste sous forme d'un string
-     * @return Description de la valeur retournée
+     * @return la Liste sous forme de string
      */
     public String toString() {
         StringBuilder sb = new StringBuilder("ListeSimple(");
@@ -62,10 +73,11 @@ public class ListeSimple {
         sb.append(")");
         return sb.toString();
     }
+
     /**
      * Description de la méthode
      * suprime le premier noeud de la tete
-     * @return Description de la valeur retournée
+     * @param element supprime le premier maillon qui a cet élément
      */
     public void supprimePremier(Object element) {
         if (tete != null) {
@@ -86,18 +98,22 @@ public class ListeSimple {
             }
         }
     }
+
     /**
      * Description de la méthode
      * inisialise la supresion de tout la liste recurcivement
-     * @return Description de la valeur retournée
+     * @param element supprime tous les maillons qui ont cet élément
      */
     public void supprimeTous(int element) {
-       tete = supprimeTousRecurs(element, tete);
+        tete = supprimeTousRecurs(element, tete);
     }
+
     /**
      * Description de la méthode
      * suprime tout les noeud de la liste recurcivement
-     * @return Description de la valeur retournée
+     * @param element l'élément à supprimer
+     * @param tete la tête de la sous-liste
+     * @return le maillon suivant ou null
      */
     public Noeud supprimeTousRecurs(Object element, Noeud tete) {
         if (tete != null) {
@@ -111,10 +127,11 @@ public class ListeSimple {
             }
         } else return null;
     }
+
     /**
      * Description de la méthode
      * recupére l'avant dernier noeud de la liste
-     * @return Description de la valeur retournée
+     * @return le maillon avant dernier maillon
      */
     public Noeud getAvantDernier() {
         if (tete == null || tete.getSuivant() == null)
@@ -129,10 +146,10 @@ public class ListeSimple {
             return courant;
         }
     }
+
     /**
      * Description de la méthode
      * inverse l'ordre de la liste
-     * @return Description de la valeur retournée
      */
     public void inverser() {
         Noeud precedent = null;
@@ -145,13 +162,14 @@ public class ListeSimple {
         }
         tete = precedent;
     }
+
     /**
      * Description de la méthode
      * recupére le noeud precedent
-     * @return Description de la valeur retournée
+     * @param r le noeud dont on veut le précédent
+     * @return le maillon precedent
      */
     public Noeud getPrecedent(Noeud r) {
-    // la liste n'est pas vide puisqu'on transmet un Node de la liste et le Node existe obligatoirement
         Noeud precedent = tete;
         Noeud courant = precedent.getSuivant();
         while (courant != r) {
@@ -160,14 +178,15 @@ public class ListeSimple {
         }
         return precedent;
     }
+
     /**
      * Description de la méthode
      * echange de position entre les 2 noeud de la liste
-     * @return Description de la valeur retournée
+     * @param r1 premier noeud à échanger
+     * @param r2 second noeud à échanger
      */
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 != r2){
-
             Noeud precedentR1;
             Noeud precedentR2;
             if (r1 != tete && r2 != tete) {
@@ -191,5 +210,4 @@ public class ListeSimple {
             r1.setSuivant(temp);
         }
     }
-
 }
--- src/main/java/liste/Noeud.java
+++ src/main/java/liste/Noeud.java
@@ -1,30 +1,63 @@
 package liste;
-
+/**
+ * Représente un noeud.
+ */
 public class Noeud {
     private Object element;
     private Noeud suivant;
 
+    /**
+     * Description de la méthode
+     * constructeur du noeud
+     * @param e l'élément à stocker dans le noeud
+     * @param suivant le noeud suivant
+     */
     public Noeud(int e, Noeud suivant) {
         element = e;
         this.suivant = suivant;
     }
 
+    /**
+     * Description de la méthode
+     * recupére l'element du noeud
+     * @return element l'élément à recup
+     */
     public Object getElement() {
         return element;
     }
 
+    /**
+     * Description de la méthode
+     * ajoute/change un element dans le noeud
+     * @param element l'élément à définir
+     */
     public void setElement(Object element) {
         this.element = element;
     }
 
+    /**
+     * Description de la méthode
+     * recupére le noeud suivant
+     * @return suivant noeud suivant
+     */
     public Noeud getSuivant() {
         return suivant;
     }
 
+    /**
+     * Description de la méthode
+     * ajoute/change un noeud suivant du noeud
+     * @param suivant le noeud suivant à définir
+     */
     public void setSuivant(Noeud suivant) {
         this.suivant = suivant;
     }
 
+    /**
+     * Description de la méthode
+     * affiche le noeud sous la forme d'un string
+     * @return le Noeud sous forme de string
+     */
     public String toString() {
         return "Noeud(" + element + ")";
     }
